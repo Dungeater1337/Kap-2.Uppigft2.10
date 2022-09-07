7c3d67bbc72ab2a8c54ccdcd171d5ef47a1d239f
EmilWallett <nhlpool27@gmail.com>
2022-09-07 12:01:32 +0200

    Add project files.

---
+++ Kap 2.Uppigft2.10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+namespace Kapitel_2_uppgirft_2_10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken stad kommer du ifrån?"); 
+            string stad = Console.ReadLine();
+            Console.WriteLine("Vart är ditt drömresmål?"); 
+            string resmål = Console.ReadLine();
+
+           Console.WriteLine("Hej " + stad + "sbo. Jag hoppas att du får resa till " + resmål + ".");
+
+
+
+
+
+
+
+        }
+       
+
+    }
+
+
+}
+
+
+
